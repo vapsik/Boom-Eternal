44044fd6a7b496c367ce9336d0fcf25056e42855
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2022-12-15 21:55:02 +0200

    test commit

---
+++ Boom Eternal/Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    // Kui sa seda loed sa oled lol
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
