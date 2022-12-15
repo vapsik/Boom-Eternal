1ea62dce82bb09a24c950fd54a89487d0773a074
vapsik <95504719+vapsik@users.noreply.github.com>
2022-12-15 21:02:21 +0200

    Väike asi ja värki

---
+++ Boom Eternal/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float inputX;
+    float inputY;
+
+    public float speed;
+    Rigidbody2D rb;
+    void Awake() {
+        rb = gameObject.GetComponent<Rigidbody2D>();   
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+    }
+    
+    void FixedUpdate() {
+        //fikseeritud max kiirusega
+        rb.velocity = new Vector2(inputX, inputY).normalized * speed; 
+    }
+}
