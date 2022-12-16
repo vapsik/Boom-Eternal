94dc71904890edb59cb5b840dcc4e3112013190f
vapsik <95504719+vapsik@users.noreply.github.com>
2022-12-16 20:30:13 +0200

    static script
    lisasin static scripti globalreferences, milles hoiustame väärtusi ja objekte, mida stseenist-stseeni alles hoida

---
+++ Boom Eternal/Assets/Scripts/GlobalReferences.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GlobalReferences 
+{
+    public static int hp;
+    public static int score;
+    public static GameObject enemyPrefab1;
+    public static GameObject enemyPrefab2;
+}
--- Boom Eternal/Assets/Scripts/TestScript.cs
+++ Boom Eternal/Assets/Scripts/TestScript.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class TestScript : MonoBehaviour
 {
 
+    public GameObject vastasePrefab1;
     // Kui sa seda loed sa oled lol
 
     // Start is called before the first frame update
+    void Awake() {
+        if(vastasePrefab1 != null)
+            GlobalReferences.enemyPrefab1 = vastasePrefab1;    
+    }
     void Start()
     {
         
