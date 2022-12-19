74a18111f41b9b61a0474658b4388b55b6fc604e
vapsik <95504719+vapsik@users.noreply.github.com>
2022-12-19 11:11:04 +0200

    veel väikseid parandus
    -kuulid jälgivad täpselt barrel-crosshair sihti
    -crosshairi püsib nüüd täielikult ekraanis
    -kõik scriptid olgu Scripts folderis

--- Boom Eternal/Assets/Scripts/AimingAndShooting.cs
+++ Boom Eternal/Assets/Scripts/AimingAndShooting.cs
@@ -16,7 +16,7 @@ public class AimingAndShooting : MonoBehaviour
     private Vector3 crosshairPosition, centerOfTheCanvas = Vector3.zero; 
 
     [HideInInspector]
-    public Vector2 aimingVector; // on ühikvektor, mis näitab, mis suunas sihitakse
+    public Vector2 aimingVector, aimingVectorFromBarrel; // on ühikvektor, mis näitab, mis suunas sihitakse
     [HideInInspector]
     public float aimingProgress; // näitab, mis murdosa maksimaalsest keskpunkti-crosshairi nihkest on praegu saavutatud
 
@@ -31,12 +31,11 @@ public class AimingAndShooting : MonoBehaviour
         resolutionX = GlobalReferences.mainCamera.pixelWidth;
         resolutionY = GlobalReferences.mainCamera.pixelHeight;
 
-        //Debug.Log(resolutionX);
-        //Debug.Log(resolutionY);    
-        //Debug.Log(crosshairPosition);
-
         mouseX = centerOfTheCanvas.x = resolutionX * 0.5f;
-        mouseY= centerOfTheCanvas.y = resolutionY * 0.5f;
+        mouseY = centerOfTheCanvas.y = resolutionY * 0.5f;
+        //väike offset algusesse:
+        mouseX += 50f;
+        mouseY += 50f;
     }
 
     // Update is called once per frame
@@ -44,26 +43,32 @@ public class AimingAndShooting : MonoBehaviour
     {
         //järgnev kood töötab, sest Canvas UI Scale Mode = Scale With Screen Size
         mouseX += sensX * Time.deltaTime * Input.GetAxis("Mouse X");
-        mouseX = Mathf.Clamp(mouseX, 0, resolutionX);
+        mouseX = Mathf.Clamp(mouseX, 8f, resolutionX - 8f);
         mouseY += sensY * Time.deltaTime * Input.GetAxis("Mouse Y");
-        mouseY = Mathf.Clamp(mouseY, 0, resolutionY);
+        mouseY = Mathf.Clamp(mouseY, 8f, resolutionY - 8f);
         
         crosshairPosition.x = mouseX;
         crosshairPosition.y = mouseY;
         crosshair.rectTransform.position = crosshairPosition;
         
-        Vector2 playerPosOnCanvas = GlobalReferences.mainCamera.WorldToScreenPoint(transform.position);       
+        Vector2 playerPosOnCanvas = GlobalReferences.mainCamera.WorldToScreenPoint(transform.position);
+        Vector2 barrelPosOnCanvas = GlobalReferences.mainCamera.WorldToScreenPoint(gunBarrel.position);
         //kui vaja kasutada crosshairi reaalset positisooni:
         //Vector3 crosshairRealPosition = GlobalReferences.mainCamera.ScreenToWorldPoint(crosshair.rectTransform.position);
-        
+
         aimingVector = new Vector2(crosshairPosition.x - playerPosOnCanvas.x, crosshairPosition.y - playerPosOnCanvas.y).normalized;
         aimingProgress = (crosshairPosition - centerOfTheCanvas).magnitude/(centerOfTheCanvas.magnitude); 
         //Debug.Log(aimingProgress);
 
-        // tulistamise osa:
+        //tulistamise osa:
         if(Input.GetKeyDown(KeyCode.Mouse0)){
+            aimingVectorFromBarrel = new Vector2(crosshairPosition.x - barrelPosOnCanvas.x, crosshairPosition.y - barrelPosOnCanvas.y).normalized;
+            //selline instantiate'imine toimib ainult puhul ümara kuuli puhul: 
             GameObject bullet = Instantiate(testBulletPrefab, gunBarrel.position, Quaternion.identity);
-            bullet.GetComponent<Rigidbody2D>().velocity = aimingVector * 12f;
+            bullet.GetComponent<Rigidbody2D>().velocity = aimingVectorFromBarrel * 12f;
+            ////kuulikujulise kuuli puhul:
+            //GameObject bullet = Instantiate(testBulletPrefab, gunBarrel.position, Quaternion.identity);
+            //bullet.transform.LookAt(transform.position + 10000f * aimingVectorFromBarrel); //(?)
         }
     }
 }
